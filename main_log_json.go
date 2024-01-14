dfa1b5b6392656f68bca426b610afc8edab60e9e
lei <random2035@qq.com>
2024-01-14 12:45:36 +0800

    2

--- main_log_json.go
+++ main_log_json.go
@@ -12,6 +12,7 @@ func main() {
 		for {
 			i++
 			time.Sleep(time.Second)
+			// 实际项目中最好在日志中使用毫秒级的时间，这里仅精确到秒
 			fmt.Println(fmt.Sprintf(`{"time": "%s", "number": %d, "field1":"abcdefghijklmn","field2":"0123456789","field3":"Golang","field4":"Kubernetes"}`, time.Now().Format(time.DateTime), i))
 		}
 	}()
