f398e209f36ad78f1cb545d174265f12733c667d
lei <random2035@qq.com>
2024-01-18 13:31:46 +0800

    1

--- k8s_actions_guide/version1/main_multiroute.go
+++ k8s_actions_guide/version1/main_multiroute.go
@@ -26,6 +26,21 @@ func main() {
 			fmt.Fprintf(w, "Hello, You are at %s, Got: %s", _route, _resp)
 		})
 	}
+
+	// 模拟连接数据库操作
+	http.HandleFunc("/connect_db", func(w http.ResponseWriter, r *http.Request) {
+		dbpass := os.Getenv("DB_PASS")
+
+		if dbpass == "" { // 是否读取到配置
+			fmt.Fprintf(w, "Sorry, no db password provided!")
+			return
+		} else if dbpass != "pass123" { // 验证密码
+			fmt.Fprintf(w, "Sorry, wrong db password provided!")
+		}
+
+		// 连接成功
+		fmt.Fprintf(w, "Hello, You are connected database successfully!")
+	})
 	log.Printf("Listening on http://localhost:3000\n")
 	panic(http.ListenAndServe(":3000", nil))
 }
